9516b312ce39a1d51af201c87c894df0088a0646
HasanShaddad <egyptioncoder@gmail.com>
2017-01-17 03:25:56 +0200

    add project files
    add project files - by hassan shaddad 17-1-2017

---
+++ Store1/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Store1.Startup))]
+namespace Store1
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
